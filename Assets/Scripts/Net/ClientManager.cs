8ae5b129a98de44219df89f702e78dcf1e87d6eb
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-03-19 16:43:17 +0800

    IP和端口号的确定

--- Assets/Scripts/Net/ClientManager.cs
+++ Assets/Scripts/Net/ClientManager.cs
@@ -1,8 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.Sockets;
+using System;
 
 public class ClientManager : BaseManager<ClientManager>
 {
+    private const string ip = "127.0.0.1";
+    private const int port = 2333;
 
+    private Socket clientSocket;
+
+    public override ClientManager OnInit()
+    {
+        clientSocket = new Socket(AddressFamily.InterNetwork
+            , SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            clientSocket.Connect(ip, port);
+        }
+        catch(Exception e)
+        {
+            Debug.Log("连接socket失败："+e);
+        }
+        return base.OnInit();
+    }
+
+    public override void OnDesotry()
+    {
+        base.OnDesotry();
+        try
+        {
+            clientSocket.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("关闭socket失败：" + e);
+        }
+    }
 }
